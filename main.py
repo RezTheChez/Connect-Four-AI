4a1a0c731b5d766d82a77487fd8b0f046f33ef32
RezTheChez <103398512+RezTheChez@users.noreply.github.com>
2023-05-24 15:17:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame, sys
 from element import *
 from connectFour import *
+from pygame.locals import *
 
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 700))
+screen = pygame.display.set_mode((700, 700))
 pygame.display.set_caption("Menu")
 skill = 30
 defensiveness = 30
@@ -13,7 +14,7 @@ aggressiveness = 30
 outputDefensiveness = 1
 outputAggressiveness = 1
 depth = 1
-settingsPage = 1
+invertMinimax = False
 
 def getFont(size):
     return pygame.font.SysFont("calibri", size, bold=True)
@@ -23,7 +24,7 @@ def renderText(screen, color, text, xPos, yPos, size):
     screen.blit(label, (xPos, yPos))
 
 def Button2(screen, x, y, width, height, color, action=None):
-    global skill, defensiveness, aggressiveness
+    global skill, defensiveness, aggressiveness, invertMinimax
     mousePos = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
@@ -40,7 +41,7 @@ def Button2(screen, x, y, width, height, color, action=None):
 
 def play():
     pygame.display.set_caption("Connect Four")
-    startGame(depth, outputDefensiveness, outputAggressiveness)
+    startGame(depth, outputDefensiveness, outputAggressiveness, invertMinimax)
     mainMenu()
 
 
@@ -51,52 +52,20 @@ def settings():
         screen.fill("black")
         mousePos = pygame.mouse.get_pos()
 
-        if settingsPage == 1:
-          optionsText = getFont(75).render("AI Settings: ", True, "White")
-          optionsRect = optionsText.get_rect(center=(350, 50))
-          skillText = getFont(45).render("Skill: ", True, "White")
-          skillRect = optionsText.get_rect(center=(300, 150))
-          defensivnessText = getFont(45).render("Defensiveness: ", True, "White")
-          defensivnessRect = optionsText.get_rect(center=(300, 300))
-          aggressivenessText = getFont(45).render("Aggressiveness: ", True, "White")
-          aggressivenessRect = optionsText.get_rect(center=(300, 450))
-          screen.blit(optionsText, optionsRect)
-          screen.blit(skillText, skillRect)
-          screen.blit(defensivnessText, defensivnessRect)
-          screen.blit(aggressivenessText, aggressivenessRect)
-        elif settingsPage == 2:
-          optionsText = getFont(75).render("Visual Settings: ", True, "White")
-          optionsRect = optionsText.get_rect(center=(350, 50))
-          skillText = getFont(45).render("Evaluation Bar", True, "White")
-          skillRect = optionsText.get_rect(center=(200, 150))
-          screen.blit(optionsText, optionsRect)
+        optionsText = getFont(75).render("AI Settings: ", True, "White")
+        optionsRect = optionsText.get_rect(center=(350, 50))
+        skillText = getFont(45).render("Skill: ", True, "White")
+        skillRect = optionsText.get_rect(center=(200, 100))
+        defensivnessText = getFont(45).render("Defensiveness: ", True, "White")
+        defensivnessRect = optionsText.get_rect(center=(200, 250))
+        aggressivenessText = getFont(45).render("Aggressiveness: ", True, "White")
+        aggressivenessRect = optionsText.get_rect(center=(200, 400))
+        screen.blit(optionsText, optionsRect)
+        screen.blit(skillText, skillRect)
+        screen.blit(defensivnessText, defensivnessRect)
+        screen.blit(aggressivenessText, aggressivenessRect)
 
-        optionsBack = Button(image=None, pos=(350, 630),
-                             textInput="BACK", font=getFont(75), baseColor="White", hoveringColor="Green")
-
-        if settingsPage == 1:
-          # Skill
-          Button2(screen, 30, 200, 400, 2, (255, 255, 255), action="scroll1")
-          pygame.draw.rect(screen, (255, 255, 255), [skill, 200 - 12, 10, 24])
-          depth = round((skill / 100 + 0.7), 0)
-          renderText(screen, (255, 255, 255), str(depth), 470, 180, 45)
-  
-          # Defensiveness
-          Button2(screen, 30, 350, 400, 2, (255, 255, 255), action="scroll2")
-          pygame.draw.rect(screen, (255, 255, 255), [defensiveness, 350 - 12, 10, 24])
-          outputDefensiveness = round((defensiveness / 100 + 0.7), 0)
-          renderText(screen, (255, 255, 255), str(outputDefensiveness), 470, 330, 45)
-  
-          # Agressiveness
-          Button2(screen, 30, 500, 400, 2, (255, 255, 255), action="scroll3")
-          pygame.draw.rect(screen, (255, 255, 255), [aggressiveness, 500 - 12, 10, 24])
-          outputAggressiveness = round((aggressiveness / 100 + 0.7), 0)
-          renderText(screen, (255, 255, 255), str(outputAggressiveness), 470, 480, 45)
-        elif settingsPage == 2:
-          evalBarToggle = Button(image=pygame.image.load("menuRectangle.png"), pos=(400, 150), textInput="", font=getFont(75), baseColor="White", hoveringColor="Green")
-
-          evalBarToggle.changeColor(mousePos)
-          evalBarToggle.update(screen)
+        optionsBack = Button(None, (350, 630), "BACK", getFont(75), "White", "Green")
 
         optionsBack.changeColor(mousePos)
         optionsBack.update(screen)
@@ -109,11 +78,29 @@ def settings():
                 if optionsBack.checkForInput(mousePos):
                     mainMenu()
 
+        # Skill
+        Button2(screen, 30, 150, 400, 2, (255, 255, 255), action="scroll1")
+        pygame.draw.rect(screen, (255, 255, 255), [skill, 150 - 12, 10, 24])
+        depth = round((skill / 100 + 0.7), 0)
+        renderText(screen, (255, 255, 255), str(depth), 480, 125, 45)
+
+        # Defensiveness
+        Button2(screen, 30, 300, 400, 2, (255, 255, 255), action="scroll2")
+        pygame.draw.rect(screen, (255, 255, 255), [defensiveness, 300 - 12, 10, 24])
+        outputDefensiveness = round((defensiveness / 100 + 0.7), 0)
+        renderText(screen, (255, 255, 255), str(outputDefensiveness), 480, 275, 45)
+
+        # Agressiveness
+        Button2(screen, 30, 450, 400, 2, (255, 255, 255), action="scroll3")
+        pygame.draw.rect(screen, (255, 255, 255), [aggressiveness, 450 - 12, 10, 24])
+        outputAggressiveness = round((aggressiveness / 100 + 0.7), 0)
+        renderText(screen, (255, 255, 255), str(outputAggressiveness), 480, 425, 45)
+
         pygame.display.update()
 
 def mainMenu():
     global screen
-    screen = pygame.display.set_mode((800, 700))
+    screen = pygame.display.set_mode((700, 700))
 
     while True:
         screen.fill("black")
@@ -124,12 +111,9 @@ def mainMenu():
         menuText2 = getFont(100).render("Main Menu", True, (255, 255, 255))
         menuRect2 = menuText2.get_rect(center=(375, 150))
 
-        playButton = Button(image=pygame.image.load("menuRectangle.png"), pos=(350, 300),
-                            textInput="Play", font=getFont(75), baseColor="White", hoveringColor="Green")
-        settingsButton = Button(image=pygame.image.load("menuRectangle.png"), pos=(350, 450),
-                               textInput="Settings", font=getFont(75), baseColor="White", hoveringColor="Green")
-        quitButton = Button(image=pygame.image.load("menuRectangle.png"), pos=(350, 600),
-                            textInput="Quit", font=getFont(75), baseColor="White", hoveringColor="Green")
+        playButton = Button(pygame.image.load("menuRectangle.png"), (350, 300), "Play", getFont(75), "White", "Green")
+        settingsButton = Button(pygame.image.load("menuRectangle.png"), (350, 450), "Settings", getFont(75), "White", "Green")
+        quitButton = Button(pygame.image.load("menuRectangle.png"), (350, 600), "Quit", getFont(75), "White", "Green")
 
         screen.blit(menuText1, menuRect1)
         screen.blit(menuText2, menuRect2)
